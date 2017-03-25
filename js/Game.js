d31040d41da098df33f7f35918ab12edcf5cef99
Andreas Hammer <andreas.hammer.97@protonmail.com>
2017-03-25 16:10:13 +0100

    Tutorial

---
+++ js/Game.js
@@ -0,0 +1,46 @@
+var Game = {
+    width: 640,
+    height: 640
+};
+
+
+/*
+var a = [4];
+a.push(1);
+a.push([1, 2]);
+console.log("");
+for(var i = 0; i < 4; i++){
+
+}
+if(a < b){
+
+}
+function sum(a, b){
+    return a + b;
+}
+
+var obj = {
+    name: "Hei",
+    age: 2,
+    toString: function(){
+        return this.name + this.age;
+    }
+};
+
+obj.g = 7;
+
+function Portal(){
+    this.x = 5;
+    this.y = 1;
+
+    this.teleport = function(){
+
+    };
+}
+
+Portal.prototype.hei = function(){
+
+};
+
+var b = new Portal();
+b.hei();*/

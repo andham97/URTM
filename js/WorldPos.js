39908c478f06550b77f4c60f533f4b83a37939d5
Andreas Hammer <andreas.hammer.97@protonmail.com>
2017-03-25 16:50:11 +0100

    getters for worldpos

--- js/WorldPos.js
+++ js/WorldPos.js
@@ -2,3 +2,19 @@ function WorldPos(x, y){
     this.x = x * Game.tileSize;
     this.y = y * Game.tileSize;
 }
+
+WorldPos.prototype.getX = function(){
+    return this.x / Game.tileSize;
+};
+
+WorldPos.prototype.getY = function(){
+    return this.Y / Game.tileSize;
+};
+
+WorldPos.prototype.getRealX = function(){
+    return this.x;
+};
+
+WorldPos.prototype.getRealY = function(){
+    return this.y;
+};

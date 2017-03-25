ec7a9338220584b27ecd76b2b7828a868781411f
trulsmt <trulsmt@stud.ntnu.no>
2017-03-25 17:47:41 +0100

    added functionality in Men and Monster

--- js/Men.js
+++ js/Men.js
@@ -2,22 +2,30 @@
  * Created by Truls on 25.03.2017.
  */
 function Men() {
-    number: 5;
+    number = 5;
+    menWithSwords = null;
+    menWithGuns = null;
+
     if(number % 2 == 0) {
-        menWithSwords: this.number / 2;
-        menWithGuns: this.number / 2
+        menWithSwords = this.number / 2;
+        menWithGuns =  this.number / 2
 
     } else {
-        menWithSwords: this.number / 2;
-        menWithGuns: this.number / 2 + 1;
+        menWithSwords = this.number / 2;
+        menWithGuns = this.number / 2 + 1;
     }
+    this.a = people[number];
+    for(i = 0; i < menWithSwords; i++) {
+        this.prototype.a.push(menWithSwords);
+    }
+    this.prototype.a.push(1);
 };
 
-Men.move = function () {
-
+Men.prototype.move = function (manNumber, x, y) {
+    a[manNumber] = new WorldPos(x, y);
 }
 
-Men.kill = function () {
-
+Men.prototype.kill = function () {
+    Monster.prototype.kill();
 }
 
